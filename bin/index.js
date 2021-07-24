13aed6519b25c0b2b9e7f3086747e1af8efb47c1
tonalmathew <tonalmathew1999@gmail.com>
2021-07-24 19:36:36 +0530

    chor: setup

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Hello, ${options.name}! Wekcome to static-cli`;
+
+console.log(greeting);
