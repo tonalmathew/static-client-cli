f94ad72d79b4e221b12ab4fb0a6b0f40bdf1f89e
rubenshibu <rubenshibu@gmail.com>
2021-07-25 22:12:06 +0530

    chore: added commander

--- bin/static.js
+++ bin/static.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 
-const figlet = require('figlet');
-const chalk = require('chalk');
+const figlet = require("figlet");
+const chalk = require("chalk");
 
+// add commander for cli
+const commander = require("commander");
+
+commander
+  .command("start")
+  .descryption("start the server")
+  .action(() => {
+    console.log("starting");
+  })
+  .version("0.0.1")
+  .option(
+    "-e, --environment [environment]",
+    "Environment to work with Static Cli by Terminal"
+  )
+  .option("-n, --name[name]", "The name", "admin")
+  .action((options) => {
+    console.log(options.environment);
+    console.log(
+      chalk.blue(
+        figlet.textSync(options.environment, { horizontalLayout: "full" })
+      )
+    );
+  });
 console.clear();
 
-console.log(
-  chalk.red(figlet.textSync('STATIC CLI'))
-);
-
+// console.log(chalk.red(figlet.textSync("STATIC CLI")));
+commander.parse(process.argv);
