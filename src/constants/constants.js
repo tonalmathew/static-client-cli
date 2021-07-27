3a95f22c2a05743b6779a19194f3ae230e0dd9e3
tonalmathew <tonalmathew1999@gmail.com>
2021-07-27 15:14:24 +0530

    chor: logger subs

---
+++ src/constants/constants.js
@@ -0,0 +1,8 @@
+/**
+ * @type {String} appName - App Name
+ * @type {String} tagLine - Tagline for the app
+ *
+ */
+
+export const appName = "STATIC CLIENT";
+export const tagLine = "Faster way to setup your static projects 🚀\n";
