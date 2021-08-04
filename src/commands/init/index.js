0bfe87092bc1e479d7b9d572531bbb7b75ecad6a
tonalmathew <tonalmathew1999@gmail.com>
2021-08-04 11:27:54 +0530

    chore: add commander

---
+++ src/commands/init/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var inquirer = require("inquirer");
+
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "list",
+//       message: "Select the CSS framework which you want to use: ",
+//       name: "framework",
+//       choices: ["Bootstrap", "tailwind", "Materialize"],
+//     },
+//   ])
+//   .then((answers) => {
+//     console.log(answers);
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       console.error(error.message);
+//     } else {
+//       console.error(error.stack);
+//     }
+//   });
